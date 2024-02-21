e2aa46e9f880e59921ceb34e2f2a279caa0614d9
Matthias Fripp <mfripp@edf.org>
2024-02-21 14:00:29 -0800

    add setup_cases.sh script

---
+++ setup_cases.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python pg_to_switch.py MIP_results_comparison/case_settings/26-zone/settings switch/26-zone/in --case-id base_short --case-id base_short_current_policies --case-id base_short_no_ccs --myopic
+python pg_to_switch.py MIP_results_comparison/case_settings/26-zone/settings switch/26-zone/in --case-id base_short
